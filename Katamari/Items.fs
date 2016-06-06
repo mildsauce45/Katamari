58d2face968c38a0a0b590df9a7dddd08bd89372
mildsauce45 <jmpinchot@gmail.com>
2016-06-06 09:40:57 -0700

    fixed some typos, lower cased all item names

--- Katamari/Items.fs
+++ Katamari/Items.fs
@@ -3,16 +3,16 @@
 
         open GameData
 
-        let banana = { name="Banana"; description="You don't need to be a monkey to eat this."; size=10.0 }
-        let eraser = { name="Eraser"; description="Rub and erase. But it makes a mess."; size=8.0 }
-        let strawberry = { name="Stawberry"; description="Don't try to eat the whole ting in one bite!"; size=10.8 }
-        let wcrayon = { name="White Crayon"; description="A white crayon. Using this on white paper is pointless"; size=5.9 }
-        let bcrayon = { name="Black Crayon"; description="A black crayon. Good for drawing the night sky."; size=3.7 }
-        let rcrayon = { name="Red Crayon"; description="A red crayon. This is not lipstick. Grils, be careful."; size=4.2 }
+        let banana = { name="banana"; description="You don't need to be a monkey to eat this."; size=10.0 }
+        let eraser = { name="eraser"; description="Rub and erase. But it makes a mess."; size=8.0 }
+        let strawberry = { name="strawberry"; description="Don't try to eat the whole thing in one bite!"; size=10.8 }
+        let wcrayon = { name="white crayon"; description="A white crayon. Using this on white paper is pointless."; size=5.9 }
+        let bcrayon = { name="black crayon"; description="A black crayon. Good for drawing the night sky."; size=3.7 }
+        let rcrayon = { name="red crayon"; description="A red crayon. This is not lipstick. Girls, be careful."; size=4.2 }
         let coin = { name="5 yen"; description="People believe this will bring them good luck."; size=2.8 }
-        let hairpin = { name="Hairpin"; description="Used to fix hairstyles. Also teh name for a sharp curve, but you can't put that on your head."; size=4.5 }
-        let pachinko = { name="Pachinko Ball"; description="The usually disappear very quickly when playing Pachinko..."; size=4.7 }
-        let mahjong = { name="Mah-Jong Tile"; description="This is from a game where you do a lot of loud shuffling. "; size=5.1 }
-        let _match = { name="Match"; description="Some people are so skillful that they can build a small house with this."; size=1.6 }
-        let frog = { name="Tree Frog"; description="A little frog. When it was young, it didn't have arms."; size=7.4 }
-        let screw = { name="Short Screw"; description="You turn this thing, and it will hold other things"; size=2.5 }
+        let hairpin = { name="hairpin"; description="Used to fix hairstyles. Also the name for a sharp curve, but you can't put that on your head."; size=4.5 }
+        let pachinko = { name="pachinko ball"; description="They usually disappear very quickly when playing Pachinko..."; size=4.7 }
+        let mahjong = { name="mah-jong tile"; description="This is from a game where you do a lot of loud shuffling. "; size=5.1 }
+        let _match = { name="match"; description="Some people are so skillful that they can build a small house with this."; size=1.6 }
+        let frog = { name="tree frog"; description="A little frog. When it was young, it didn't have arms."; size=7.4 }
+        let screw = { name="short screw"; description="You turn this thing, and it will hold other things."; size=2.5 }
